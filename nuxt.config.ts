5d0581b5a2379545551110fbd1030f69ff21f5d1
Bitti <Bitti09@users.noreply.github.com>
2024-01-09 21:28:22 +0100

    Add nuxt-api-party module and configure API endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui','nuxt-api-party'],
   ui: {
     icons: ['heroicons', 'simple-icons']
+  },
+  apiParty: {
+    endpoints: {
+      wfstatus: {
+        url: "https://api.warframestat.us",
+        // Global headers sent with each request
+      }
+    }
   },
   // Devtools / Typescript
   devtools: { enabled: true },
